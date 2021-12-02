c77c054d87f78327ed5d7b02d166553d80ff93e4
sanaz <sanaz.keyvanloo1992@gmail.com>
2021-12-03 02:13:31 +0330

    check auth for channels

--- src/config/cors.php
+++ src/config/cors.php
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie','sanctum/*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
--- src/routes/v1/channel_routes.php
+++ src/routes/v1/channel_routes.php
@@ -8,8 +8,11 @@ use  \App\Http\Controllers\API\V1\Channel\ChannelController;
 Route::prefix('/channel')->group(function () {
 
     Route::get('/all', [ChannelController::class, 'getAllChannelsList'])->name('channel.all');
-    Route::post('/create', [ChannelController::class, 'create'])->name('channel.create');
-    Route::put('/update', [ChannelController::class, 'update'])->name('channel.update');
-    Route::delete('/delete', [ChannelController::class, 'delete'])->name('channel.delete');
+
+    Route::middleware('sanctum:auth')->group(function () {
+        Route::post('/create', [ChannelController::class, 'create'])->name('channel.create');
+        Route::put('/update', [ChannelController::class, 'update'])->name('channel.update');
+        Route::delete('/delete', [ChannelController::class, 'delete'])->name('channel.delete');
+    });
 
 });
